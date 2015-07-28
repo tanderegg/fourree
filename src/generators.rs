32d56b44186ae75ec92f3373bf3820077c96a62c
Tim Anderegg <timothy.anderegg@cfpb.gov>
2015-07-28 16:04:10 -0400

    Closes #1 by adding command line interface, plus some optimizations and more realistic benchmarks

--- src/generators.rs
+++ src/generators.rs
@@ -1,14 +1,15 @@
 extern crate rand;
 
+// use std::str::from_utf8;
 use rand::Rng;
 use rand::distributions::{IndependentSample, Range, Normal};
 
 const UPPERCASE_CHARS: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 pub struct Date {
-    day: u32,
-    month: u32,
-    year: u32
+    day: u8,
+    month: u8,
+    year: u8
 }
 
 impl ToString for Date {
@@ -37,14 +38,19 @@ pub fn generate_integer<R: rand::Rng>(rng: &mut R, min: i64, max: i64) -> i64 {
 ///
 pub fn generate_string<R: rand::Rng>(rng: &mut R, length: usize) -> String {
     let dist = Range::new(0, 26);
-    let mut result = String::new();
+    let mut result: String = String::with_capacity(length);
 
     for _ in 0..length {
         let index = dist.ind_sample(rng);
-        result.push((UPPERCASE_CHARS.char_at(index as usize)));
+        result.push(UPPERCASE_CHARS.char_at(index as usize));
     }
+    result
 
-    result.to_string()
+    // 1/15th the time, but generates any u8 character instead
+    // of just capital letters.
+    /*let mut bytes = Vec::with_capacity(length);
+    rng.fill_bytes(&mut bytes);
+    from_utf8(&bytes).unwrap().to_string()*/
 }
 
 /// Generates an integer from a normal (Gaussian) distribution
@@ -52,9 +58,10 @@ pub fn generate_string<R: rand::Rng>(rng: &mut R, length: usize) -> String {
 /// # Examples
 ///
 /// let x = generate_gauss(&mut rng, 10, 2);
-pub fn generate_gauss<R: rand::Rng>(rng: &mut R, mean: f64, std_dev: f64) -> f64 {
-    let dist = Normal::new(mean, std_dev);
-    dist.ind_sample(rng)
+///
+pub fn generate_gauss<R: rand::Rng>(rng: &mut R, mean: u64, std_dev: u64) -> u64 {
+    let dist = Normal::new(mean as f64, std_dev as f64);
+    dist.ind_sample(rng) as u64
 }
 
 /// Generates a date (as a string for now)
@@ -63,20 +70,25 @@ pub fn generate_gauss<R: rand::Rng>(rng: &mut R, mean: f64, std_dev: f64) -> f64
 ///
 /// let x = generate_date(&mut rng);
 ///
-// TODO: Perhaps convert to generate a date as a single int in seconds,
-// then convert to a proper date type using a crate for dates.
 pub fn generate_date<R: rand::Rng>(rng: &mut R) -> Date {
-    let year = generate_integer(rng, 1900, 2016);
-    let month = generate_integer(rng, 1, 12);
-    let day = match month {
-        1 | 3 | 5 | 7...8 | 10 | 12 => generate_integer(rng, 1, 31),
-        2 => generate_integer(rng, 1, 28),
-        _ => generate_integer(rng, 1, 30)
+    let mut bytes = [0u8; 3];
+    rng.fill_bytes(&mut bytes);
+
+    let month: u8 = (bytes[0] * 12) / 255;
+    let year: u8 = (bytes[1] * 116) / 255;
+
+    let day_range = match month {
+        1 | 3 | 5 | 7...8 | 10 | 12 => 31,
+        2 => 28,
+        _ => 30
     };
+
+    let day = (bytes[2] * day_range) / 255;
+
     Date {
-        month: month as u32,
-        day: day as u32,
-        year: year as u32
+        month: month as u8,
+        day: day as u8,
+        year: year as u8
     }
 }
 
@@ -88,5 +100,5 @@ pub fn generate_date<R: rand::Rng>(rng: &mut R) -> Date {
 /// let y = generate_choice(&mut rng, &x);
 ///
 pub fn generate_choice<'a, R: rand::Rng, T>(rng: &mut R, choices: &'a [T]) -> &'a T {
-    &choices[generate_integer(rng, 0, (choices.len() as i64 - 1)) as usize]
+    &rng.choose(choices).unwrap()
 }
