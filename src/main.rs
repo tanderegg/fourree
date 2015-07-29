4127a92fedec1663568d5535449216f78a35199a
Tim Anderegg <timothy.anderegg@gmail.com>
2015-07-29 16:08:08 -0400

    Adding multithreading support for the executable

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ extern crate fourree;
 extern crate log;
 
 use std::env;
+use std::thread;
+use std::sync::Arc;
 use getopts::Options;
 use log::LogLevelFilter;
 
@@ -30,6 +32,7 @@ fn main() {
     opts.optopt("n", "num_rows", "specify number of records to generate", "NUM_ROWS");
     opts.optopt("b", "batch_size", "specify the size of each batch to be processed", "BATCH_SIZE");
     opts.optopt("l", "log_file", "specify a file to write the log to", "LOG_FILE_PATH");
+    opts.optopt("t", "threads", "specify the number of threads to use (default 1)", "NUM_THREADS");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
@@ -87,23 +90,64 @@ fn main() {
         return;
     };
 
+    let num_threads = if matches.opt_present("t") {
+        let thread_opt = matches.opt_str("t").unwrap().trim().to_string();
+        info!("Received option: threads = {}", thread_opt);
+        match thread_opt.parse::<u64>() {
+            Err(err) => {
+                warn!("{}, using default value {}", err, 1);
+                1
+            }
+            Ok(threads) => threads
+        }
+    } else {
+        1
+    };
+
     info!("Loading schema from: {:?}", input_file);
 
     let start_time = time::precise_time_s();
 
     match load_schema_from_file(&input_file) {
         Ok(schema) => {
-            let mut rng = rand::thread_rng();
-            let mut batch_start = time::precise_time_s();
 
-            for i in 1..num_rows {
-                let row = schema.generate_row(&mut rng, "\t");
-                debug!("{}", row);
-                
-                if i % batch_size == 0 {
-                    let batch_elapsed = time::precise_time_s();
-                    info!("{} rows proccessed, {} s elapsed", batch_size, batch_elapsed-batch_start);
-                    batch_start = time::precise_time_s();
+            if num_threads > 1 {
+                let batches = num_rows / batch_size;
+                let batches_per_thread = batches / num_threads;
+                let mut threads = Vec::with_capacity(num_threads as usize);
+                let schema_ref = Arc::new(schema);
+
+                for _ in 0..num_threads {
+                    let thread_schema = schema_ref.clone();
+                    threads.push(thread::spawn(move || {
+                        let mut rng = rand::thread_rng();
+                        for _ in 0..batches_per_thread.clone() {
+                            let batch_start = time::precise_time_s();
+                            for _ in 0..batch_size.clone() {
+                                let row = thread_schema.generate_row(&mut rng, "\t");
+                                debug!("{}", row);
+                            }
+                            let batch_elapsed = time::precise_time_s();
+                            info!("{} rows proccessed, {} s elapsed", batch_size, batch_elapsed-batch_start);
+                        }
+                    }));
+                }
+
+                for thread in threads {
+                    info!("{:?} completed.", thread.join().unwrap());
+                }
+            } else {
+                let mut rng = rand::thread_rng();
+                let mut batch_start = time::precise_time_s();
+                for i in 0..num_rows {
+                    let row = schema.generate_row(&mut rng, "\t");
+                    debug!("{}", row);
+
+                    if i % batch_size == 0 {
+                        let batch_elapsed = time::precise_time_s();
+                        info!("{} rows proccessed, {} s elapsed", batch_size, batch_elapsed-batch_start);
+                        batch_start = time::precise_time_s();
+                    }
                 }
             }
         }
